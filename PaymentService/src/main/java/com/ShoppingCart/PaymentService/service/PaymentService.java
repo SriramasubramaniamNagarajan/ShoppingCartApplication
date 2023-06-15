5a17be0c4b523feeccec5970807bda4b2337b8c2
SriramasubramaniamNagarajan <srirammani2798@gmail.com>
2023-06-15 18:12:02 +0530

    API Gateway Updated

---
+++ PaymentService/src/main/java/com/ShoppingCart/PaymentService/service/PaymentService.java
@@ -0,0 +1,10 @@
+package com.ShoppingCart.PaymentService.service;
+
+import com.ShoppingCart.PaymentService.model.PaymentRequest;
+import com.ShoppingCart.PaymentService.model.PaymentResponse;
+
+public interface PaymentService {
+    long doPayment(PaymentRequest paymentRequest);
+
+    PaymentResponse getPaymentDetailsByOrderId(long orderId);
+}
