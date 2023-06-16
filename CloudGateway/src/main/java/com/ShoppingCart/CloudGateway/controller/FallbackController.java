ef1b9e3d8c7542b568b4d03e208c7904f878eb81
SriramasubramaniamNagarajan <srirammani2798@gmail.com>
2023-06-16 15:35:54 +0530

    Circuit Breaker and redis updated

---
+++ CloudGateway/src/main/java/com/ShoppingCart/CloudGateway/controller/FallbackController.java
@@ -0,0 +1,20 @@
+package com.ShoppingCart.CloudGateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+    @GetMapping("/orderServiceFallBack")
+    public String orderServiceFallback(){
+        return "Order Service is Down!...";
+    }
+    @GetMapping("/productServiceFallBack")
+    public String productServiceFallback(){
+        return "Product Service is Down!...";
+    }
+    @GetMapping("/paymentServiceFallBack")
+    public String paymentServiceFallback(){
+        return "Payment Service is Down!...";
+    }
+}
--- OrderService/src/main/java/com/ShoppingCart/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/ShoppingCart/OrderService/external/client/ProductService.java
@@ -1,14 +1,23 @@
 package com.ShoppingCart.OrderService.external.client;
 
+import com.ShoppingCart.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
     @PutMapping("/reduceQuantity/{id}")
     ResponseEntity<Void> reduceQuantity(@PathVariable("id") long productId,
                                                @RequestParam long quantity);
+    default void fallback(Exception exception) {
+        throw new CustomException(
+                "Product Service is not Available",
+                "UNAVAILABLE",
+                500
+        );
+    }
 }
